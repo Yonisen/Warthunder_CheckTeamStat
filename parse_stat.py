f5a84565111dbe293887d29d2abce0ab01b2dae2
Yonisen <111464619+Yonisen@users.noreply.github.com>
2023-01-01 18:00:42 +0300

    обновление интерфейса

--- parse_stat.py
+++ parse_stat.py
@@ -22,6 +22,7 @@ from contextlib import suppress
 import psutil
 from threading import Timer
 #from time import time
+import configparser
 
 driver = 0
 def terminateChrome():
@@ -70,8 +71,8 @@ try:
             if len(divs) == 0:
                 i = np.append(i, 50)
                 i = np.append(i, 0)
-                i = np.append(i, "N/A")
-                i = np.append(i, "N/A")
+                i = np.append(i, "-")
+                i = np.append(i, "-")
                 #print(i) 
             else:
                 wins = divs[0].select('li:nth-of-type(2)')
@@ -109,8 +110,12 @@ try:
                 timesShturm = divs[1].select('li:nth-of-type(8)')
                 
                 timeIstr = timeIstr[diffNumber].text
+                if timeIstr == "N/A":
+                    timeIstr = '-'                
                 timesShturm = timesShturm[diffNumber].text
-                
+                if timesShturm == "N/A":
+                    timesShturm = '-'
+                    
                 i = np.append(i, winrate)
                 i = np.append(i, kd)
                 i = np.append(i, timeIstr)
@@ -119,8 +124,8 @@ try:
             if type(e)==TimeoutException:
                 i = np.append(i, 50)
                 i = np.append(i, 0)
-                i = np.append(i, "N/A")
-                i = np.append(i, "N/A")
+                i = np.append(i, "-")
+                i = np.append(i, "-")
                 #print(i)
             else:
                 raise e            
@@ -157,8 +162,8 @@ try:
                 if len(divs) == 0:
                     i = np.append(i, 50)
                     i = np.append(i, 0)
-                    i = np.append(i, "N/A")
-                    i = np.append(i, "N/A")
+                    i = np.append(i, "-")
+                    i = np.append(i, "-")
                     #print(i) 
                 else:   
                     wins = divs[0].select('li:nth-of-type(2)')
@@ -196,8 +201,12 @@ try:
                     timesShturm = divs[1].select('li:nth-of-type(8)')
                     
                     timeIstr = timeIstr[diffNumber].text
+                    if timeIstr == "N/A":
+                        timeIstr = '-'   
                     timesShturm = timesShturm[diffNumber].text
-                    
+                    if timesShturm == "N/A":
+                        timesShturm = '-'
+                        
                     i = np.append(i, winrate)
                     i = np.append(i, kd)
                     i = np.append(i, timeIstr)
@@ -206,8 +215,8 @@ try:
                 if type(e)==TimeoutException:
                     i = np.append(i, 50)
                     i = np.append(i, 0)
-                    i = np.append(i, "N/A")
-                    i = np.append(i, "N/A")
+                    i = np.append(i, "-")
+                    i = np.append(i, "-")
                     #print(i)
                 else:
                     raise e
@@ -281,7 +290,7 @@ try:
             # team2.append([i[0]])
         # else:
             # team1.append([i[0]])
-    print(diff[1])
+    #print(diff[1])
     teams = np.array(teams)
     findstat(diffNumber, teams, team1, team2)
     
@@ -335,148 +344,163 @@ try:
     team1 = np.sort(team1forSort, order = 'kd')[::-1]
     team2 = np.sort(team2forSort, order = 'kd')[::-1]
 
-    def selectWindow(event):
-        try:
-            toplist = []
-            winlist = []
-            def enum_callback(hwnd, results):
-                winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
-
-            win32gui.EnumWindows(enum_callback, toplist)
-            wt = [(hwnd, title) for hwnd, title in winlist if 'war thunder' in title.lower()]
-            # just grab the first window that matches
-            if wt !=[]:
-                wt = wt[0]
-                #pythoncom.CoInitializeEx(0)
-                #shell = win32com.client.Dispatch("WScript.Shell")
-                #shell.SendKeys('%')                
-                # use the window handle to set focus
-                win32gui.SetForegroundWindow(wt[0])  
-        except Exception as e:
-            if driver != 0:
-                driver.quit()
-                timeout = 3
-                t = Timer(timeout, terminateChrome)
-                t.start()                                
-            else:
-                terminateChrome()
-            file = open('error.log', 'a')
-            file.write('\n\n')
-            traceback.print_exc(file=file, chain=True)
-            traceback.print_exc()
-            file.close()                
-    
-    root = Tk()
-    root.geometry(f'1250x800+{int(root.winfo_screenwidth()/2-625)}+{int(root.winfo_screenheight()/2-400)}')
-    root.bind("<Button-1>",selectWindow)
-    root.bind("<Button-2>",selectWindow)
-    root.bind("<Button-3>",selectWindow)  
-    label1 = Label(root, text=f'{diff[1]}', font=('Roboto','14'), fg='gray')
-    label1.master.overrideredirect(True)
-    label1.master.lift()
-    label1.master.wm_attributes("-topmost", True)
-    
-    # hWindow = pywintypes.HANDLE(int(label1.master.frame(), 16)) 
-    # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
-    # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)        
-    
-    label1.pack()
     sign = 0
+    sign1 = 0
+    
     if team1winrate>team2winrate:
         sign = '>'
     elif team1winrate==team2winrate:
         sign = '='
     else:
         sign = '<'
-    label2 = Label(root, text=f'{team1winrate} {sign} {team2winrate}', font=('Roboto','19', 'bold'), fg='black')
-    label2.master.overrideredirect(True)
-    label2.master.lift()
-    label2.master.wm_attributes("-topmost", True)
-    
-    # hWindow = pywintypes.HANDLE(int(label2.master.frame(), 16)) 
-    # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
-    # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)        
-    
-    label2.pack()
-    if team1kd>team2kd:
-        sign = '>'
-    elif team1kd==team2kd:
-        sign = '='
-    else:
-        sign = '<'    
-    label3 = Label(root, text=f'{team1kd} {sign} {team2kd}', font=('Roboto','17'), fg='black')
-    label3.master.overrideredirect(True)
-    label3.master.lift()
-    label3.master.wm_attributes("-topmost", True)
 
-    # hWindow = pywintypes.HANDLE(int(label3.master.frame(), 16)) 
-    # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
-    # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
-    
-    label3.pack()
-    
-    
-    
-    columns = ("nickname", "fighter", "attacker", "% winrate", "Kills per Battle")
-    tree = ttk.Treeview(columns=columns, show="headings")
-    tree.master.overrideredirect(True)
-    tree.master.lift()
-    tree.master.wm_attributes("-topmost", True)
-    
-    # hWindow = pywintypes.HANDLE(int(tree.master.frame(), 16)) 
-    # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
-    # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)        
-    
-    tree.pack(fill=Y, expand=1, side=LEFT)
-    tree.heading("nickname", text="Ник", anchor=W)
-    tree.heading("fighter", text="Истребитель", anchor=W)
-    tree.heading("attacker", text="Штурмовик", anchor=W)
-    tree.heading("% winrate", text="% Побед", anchor=W)
-    tree.heading("Kills per Battle", text="Фраги на бой", anchor=W)
-    tree.column("#1", stretch=NO, width=140, anchor=W)
-    tree.column("#2", stretch=NO, width=100, anchor=W)
-    tree.column("#3", stretch=NO, width=100, anchor=W)
-    tree.column("#4", stretch=NO, width=85, anchor=W)
-    tree.column("#5", stretch=NO, width=105, anchor=W)
-    for i in range(len(team1)):
-        tree.insert("", END, values=(team1[i][0], team1[i][4], team1[i][5], team1[i][2], team1[i][3]))
+    if team1kd>team2kd:
+        sign1 = '>'
+    elif team1kd==team2kd:
+        sign1 = '='
+    else:
+        sign1 = '<'   
+
+    def read_config(name):
+        config = configparser.ConfigParser()
+        config.read(name, encoding='utf-8')
+        conf = []
+        conf.append(config.get("Комбинации", "показывать оверлейное окно"))
+        return conf
+    conf = read_config("buttons.ini")
+    abv = conf[0]
+    root = 0
+    if abv == "+":
+        def selectWindow(event):
+            try:
+                toplist = []
+                winlist = []
+                def enum_callback(hwnd, results):
+                    winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+
+                win32gui.EnumWindows(enum_callback, toplist)
+                wt = [(hwnd, title) for hwnd, title in winlist if 'war thunder' in title.lower()]
+                # just grab the first window that matches
+                if wt !=[]:
+                    wt = wt[0]
+                    #pythoncom.CoInitializeEx(0)
+                    #shell = win32com.client.Dispatch("WScript.Shell")
+                    #shell.SendKeys('%')                
+                    # use the window handle to set focus
+                    win32gui.SetForegroundWindow(wt[0])  
+            except Exception as e:
+                if driver != 0:
+                    driver.quit()
+                    timeout = 3
+                    t = Timer(timeout, terminateChrome)
+                    t.start()                                
+                else:
+                    terminateChrome()
+                file = open('error.log', 'a')
+                file.write('\n\n')
+                traceback.print_exc(file=file, chain=True)
+                traceback.print_exc()
+                file.close()                
+        
+        root = Tk()
+        root.geometry(f'1250x800+{int(root.winfo_screenwidth()/2-625)}+{int(root.winfo_screenheight()/2-400)}')
+        root.bind("<Button-1>",selectWindow)
+        root.bind("<Button-2>",selectWindow)
+        root.bind("<Button-3>",selectWindow)  
+        label1 = Label(root, text=f'{diff[1]}', font=('Roboto','14'), fg='gray')
+        label1.master.overrideredirect(True)
+        label1.master.lift()
+        label1.master.wm_attributes("-topmost", True)
+        
+        # hWindow = pywintypes.HANDLE(int(label1.master.frame(), 16)) 
+        # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
+        # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)        
+        
+        label1.pack()
+
+        label2 = Label(root, text=f'{team1winrate} {sign} {team2winrate}', font=('Roboto','19', 'bold'), fg='black')
+        label2.master.overrideredirect(True)
+        label2.master.lift()
+        label2.master.wm_attributes("-topmost", True)
+        
+        # hWindow = pywintypes.HANDLE(int(label2.master.frame(), 16)) 
+        # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
+        # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)        
+        
+        label2.pack()
+     
+        label3 = Label(root, text=f'{team1kd} {sign1} {team2kd}', font=('Roboto','17'), fg='black')
+        label3.master.overrideredirect(True)
+        label3.master.lift()
+        label3.master.wm_attributes("-topmost", True)
+
+        # hWindow = pywintypes.HANDLE(int(label3.master.frame(), 16)) 
+        # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
+        # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
+        
+        label3.pack()
         
         
-    columns1 = ("Kills per Battle", "% winrate", "attacker", "fighter", "nickname")
-    tree1 = ttk.Treeview(columns=columns1, show="headings")
-    tree1.master.overrideredirect(True)
-    tree1.master.lift()
-    tree1.master.wm_attributes("-topmost", True)
-    
-    # hWindow = pywintypes.HANDLE(int(tree1.master.frame(), 16)) 
-    # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
-    # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)        
-    
-    tree1.pack(fill=Y, expand=1, side=RIGHT)
-    tree1.heading("nickname", text="Ник", anchor=W)
-    tree1.heading("fighter", text="Истребитель", anchor=W)
-    tree1.heading("attacker", text="Штурмовик", anchor=W)
-    tree1.heading("% winrate", text="% Побед", anchor=W)
-    tree1.heading("Kills per Battle", text="Фраги на бой", anchor=W)
-    tree1.column("#1", stretch=NO, width=105, anchor=W)
-    tree1.column("#2", stretch=NO, width=85, anchor=W)
-    tree1.column("#3", stretch=NO, width=100, anchor=W)
-    tree1.column("#4", stretch=NO, width=100, anchor=W)
-    tree1.column("#5", stretch=NO, width=140, anchor=W)
-    for i in range(len(team2)):
-        tree1.insert("", END, values=(team2[i][3], team2[i][2], team2[i][5], team2[i][4], team2[i][0]))     
-    
-    
-    btn1 = ttk.Button(text="X", command=root.destroy, width=3)
-    btn1.master.overrideredirect(True)
-    btn1.master.lift()
-    btn1.master.wm_attributes("-topmost", True)
-    btn1.place(x=1250-33, y=3)
+        
+        columns = ("nickname", "fighter", "attacker", "% winrate", "Kills per Battle")
+        tree = ttk.Treeview(columns=columns, show="headings")
+        tree.master.overrideredirect(True)
+        tree.master.lift()
+        tree.master.wm_attributes("-topmost", True)
+        
+        # hWindow = pywintypes.HANDLE(int(tree.master.frame(), 16)) 
+        # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
+        # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)        
+        
+        tree.pack(fill=Y, expand=1, side=LEFT)
+        tree.heading("nickname", text="Ник", anchor=W)
+        tree.heading("fighter", text="Истребитель", anchor=W)
+        tree.heading("attacker", text="Штурмовик", anchor=W)
+        tree.heading("% winrate", text="% Побед", anchor=W)
+        tree.heading("Kills per Battle", text="Фраги на бой", anchor=W)
+        tree.column("#1", stretch=NO, width=140, anchor=W)
+        tree.column("#2", stretch=NO, width=100, anchor=W)
+        tree.column("#3", stretch=NO, width=100, anchor=W)
+        tree.column("#4", stretch=NO, width=85, anchor=W)
+        tree.column("#5", stretch=NO, width=105, anchor=W)
+        for i in range(len(team1)):
+            tree.insert("", END, values=(team1[i][0], team1[i][4], team1[i][5], team1[i][2], team1[i][3]))
+            
+            
+        columns1 = ("Kills per Battle", "% winrate", "attacker", "fighter", "nickname")
+        tree1 = ttk.Treeview(columns=columns1, show="headings")
+        tree1.master.overrideredirect(True)
+        tree1.master.lift()
+        tree1.master.wm_attributes("-topmost", True)
+        
+        # hWindow = pywintypes.HANDLE(int(tree1.master.frame(), 16)) 
+        # exStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TRANSPARENT
+        # win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)        
+        
+        tree1.pack(fill=Y, expand=1, side=RIGHT)
+        tree1.heading("nickname", text="Ник", anchor=W)
+        tree1.heading("fighter", text="Истребитель", anchor=W)
+        tree1.heading("attacker", text="Штурмовик", anchor=W)
+        tree1.heading("% winrate", text="% Побед", anchor=W)
+        tree1.heading("Kills per Battle", text="Фраги на бой", anchor=W)
+        tree1.column("#1", stretch=NO, width=105, anchor=W)
+        tree1.column("#2", stretch=NO, width=85, anchor=W)
+        tree1.column("#3", stretch=NO, width=100, anchor=W)
+        tree1.column("#4", stretch=NO, width=100, anchor=W)
+        tree1.column("#5", stretch=NO, width=140, anchor=W)
+        for i in range(len(team2)):
+            tree1.insert("", END, values=(team2[i][3], team2[i][2], team2[i][5], team2[i][4], team2[i][0]))     
+        
+        
+        btn1 = ttk.Button(text="X", command=root.destroy, width=3)
+        btn1.master.overrideredirect(True)
+        btn1.master.lift()
+        btn1.master.wm_attributes("-topmost", True)
+        btn1.place(x=1250-33, y=3)
 
     # timeout = 0
     # t = Timer(timeout, selectWindow)
     # t.start()
-    
     team1 = np.array(team1, dtype=[('name', np.dtype('U100')), ('team', np.dtype('U100')),('winrate', np.dtype('U100')), ('kd', np.dtype('U100')), ('time', np.dtype('U100')), ('time1', np.dtype('U100'))])
     team2 = np.array(team2, dtype=[('name', np.dtype('U100')), ('team', np.dtype('U100')),('winrate', np.dtype('U100')), ('kd', np.dtype('U100')), ('time', np.dtype('U100')), ('time1', np.dtype('U100'))])
     delta = len(team1) - len(team2)
@@ -494,8 +518,15 @@ try:
     for i in teams:
         #print([i[0][0], i[0][5], i[0][6], i[0][3], i[0][4], '1', i[1][4], i[1][3], i[1][6], i[1][5], i[1][0]])
         myTable.add_row([i[0][0], i[0][4], i[0][5], i[0][2], i[0][3], '     ', i[1][3], i[1][2], i[1][5], i[1][4], i[1][0]])
+    
+    myTable1 = PrettyTable(['',diff[1],' '])
+    myTable1.add_row([team1winrate,sign,team2winrate])
+    myTable1.add_row([team1kd,sign1,team2kd])
+    
+    print(myTable1)
     print(myTable)
-    root.mainloop()    
+    if abv == "+":
+        root.mainloop()    
     #print(team1)
     #print(team2)
                
